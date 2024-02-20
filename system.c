595701a911ff0ba71e55d7610158c0abca47f42c
gngtwhh <2784126850@qq.com>
2024-02-20 12:01:24 +0800

    tmp

--- system.c
+++ system.c
@@ -7,11 +7,8 @@
 #include "snake.h"
 
 //system
-//涉及到windows的API
-void color(int i) {//更改文字颜色
-    //SetConsoleTextAttribute是API设置控制台窗口字体颜色和背景色的函数
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i);
-}
+//使用ANSI终端控制码来控制
+
 
 //游戏中每次调用gotoxy时的参数都是根据游戏菜单字符位置/当前坐标计算好传递过来的
 void gotoxy(int x, int y) {
